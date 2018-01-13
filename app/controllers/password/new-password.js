6b9d4c6553a21b0cb041ab6c4e0517d52f362133
Hemant Satam <hsatam@gmail.com>
2018-01-13 16:22:23 +0530

    added debug statement to track action event
    Signed-off-by: Hemant Satam <hsatam@gmail.com>

--- app/controllers/password/new-password.js
+++ app/controllers/password/new-password.js
@@ -1,7 +1,7 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  alert ('in controller header...');
+
   actions: {
     createPassword: function() {
       alert ("in controller...");
