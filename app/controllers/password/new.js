2720ec54aae535ed2ceaefa5f1f3ea198721fed7
Hemant Satam <hsatam@gmail.com>
2018-01-13 15:52:24 +0530

    added form control for capturing new password and associated Controller code to save it to the database
    Signed-off-by: Hemant Satam <hsatam@gmail.com>

--- app/controllers/password/new.js
+++ app/controllers/password/new.js
@@ -1,4 +1,16 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  actions: {
+    createPassword: function() {
+      var newPassword = this.store.createRecord('password', {
+        userId: this.get('userId'),
+        password: this.get('password'),
+        applicationName: this.get('applicationName'),
+        applicationURL: this.get('applicationURL')
+      });
+      newPassword.save();
+      this.transitionTo('plist');
+    }
+  }
 });
