1bcb928fa7155cbb61a6c1284e2c9d4d00bbb52d
Hemant Satam <hsatam@gmail.com>
2018-01-13 16:23:10 +0530

    added debug statement to track action event
    Signed-off-by: Hemant Satam <hsatam@gmail.com>

--- app/controllers/password/new-password.js
+++ app/controllers/password/new-password.js
@@ -4,7 +4,7 @@ export default Controller.extend({
 
   actions: {
     createPassword: function() {
-      alert ("in controller...");
+      alert ('in controller...');
       var newPassword = this.store.createRecord('password', {
         userId: this.get('userId'),
         password: this.get('password'),
